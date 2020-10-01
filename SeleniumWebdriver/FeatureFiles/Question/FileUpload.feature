d86d36db6a6d527cf567b0f18568817a707dc517
rathr1 <rathr1@L33986WUS>
2020-09-30 20:43:35 -0700

    Fileupload

---
+++ SeleniumWebdriver/FeatureFiles/Question/FileUpload.feature
@@ -0,0 +1,14 @@
+﻿Feature: FileUpload
+	To test the file upload feature
+
+
+Scenario: Upload the file while creating bug
+	Given I navigate to the bug zila application "http://localhost/"
+	And I click on the element with locator "enter_bug"
+	And I login into the application with  usernam "rahul@bugzila.com" and password "welcome"
+	And I press on Login button
+	And I click on "Testng" link 
+	Then I click on button "attachment_false"
+	And Wait for the attachment button "data" for "30" seconds
+	And I upload the file "ExcelData.xlsx" present in Resources
+	Then logout from the application
---
+++ SeleniumWebdriver/StepDefinition/Questions/FlieUpload.cs
@@ -0,0 +1,86 @@
+﻿using OpenQA.Selenium;
+using SeleniumWebdriver.ComponentHelper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace SeleniumWebdriver.StepDefinition.Questions
+{
+    [Binding]
+    public sealed class FlieUpload
+    {
+        [Given(@"I navigate to the bug zila application ""(.*)""")]
+        public void GivenINavigateToTheBugZilaApplication(string url)
+        {
+            NavigationHelper.NavigateToUrl(url);
+        }
+
+        [Given(@"I click on the element with locator ""(.*)""")]
+        public void GivenIClickOnTheElementWithLocator(string id)
+        {
+            ButtonHelper.ClickButton(By.Id(id));
+        }
+
+        [Given(@"I login into the application with  usernam ""(.*)"" and password ""(.*)""")]
+        public void GivenILoginIntoTheApplicationWithUsernamAndPassword(string user, string pass)
+        {
+            TextBoxHelper.TypeInTextBox(By.Id("Bugzilla_login"), user);
+            TextBoxHelper.TypeInTextBox(By.Id("Bugzilla_password"), pass);
+        }
+
+        [Given(@"I press on Login button")]
+        public void GivenIPressOnLoginButton()
+        {
+            ButtonHelper.ClickButton(By.Id("log_in"));
+        }
+
+        [Given(@"I click on ""(.*)"" link")]
+        public void GivenIClickOnLink(string link)
+        {
+            LinkHelper.ClickLink(By.LinkText(link));
+        }
+
+        [Then(@"I click on button ""(.*)""")]
+        public void ThenIClickOnButton(string btn)
+        {
+            ButtonHelper.ClickButton(By.Id(btn));
+        }
+
+        [Then(@"Wait for the attachment button ""(.*)"" for ""(.*)"" seconds")]
+        public void ThenWaitForTheAttachmentButtonForSeconds(string btnId, int timeOut)
+        {
+            GenericHelper.WaitForWebElement(By.Id(btnId), TimeSpan.FromSeconds(timeOut));
+            JavaScriptExecutor.ScrollIntoViewAndClick(By.Id(btnId));
+        }
+
+
+        [Then(@"I upload the file ""(.*)"" present in Resources")]
+        public void ThenIUploadTheFilePresentInResources(string fileName)
+        {
+            var processinfo = new ProcessStartInfo()
+            {
+                FileName = "\"" + Directory.GetCurrentDirectory() + @"\Resources\FileUpload.exe" + "\"",
+                Arguments = "\"" + Directory.GetCurrentDirectory() + @"\Resources\" + fileName + "\"",
+                UseShellExecute = false
+            };
+            using (var process = Process.Start(processinfo))
+            {
+                process.WaitForExit();
+            }
+            Thread.Sleep(3000); // Just to see in UI
+        }
+
+        [Then(@"logout from the application")]
+        public void ThenLogoutFromTheApplication()
+        {
+            ButtonHelper.Logout();
+        }
+
+
+    }
+}
